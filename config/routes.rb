e3029c2500d251ed34dd48d3f56a93076b70c4a7
WalterPeck <chrislabarge2@gmail.com>
2014-07-28 18:45:26 -0400

    Add a StaticPagescontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HybridEdu::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/games"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
